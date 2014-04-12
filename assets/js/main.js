dab7c9b7958004f2feea468ed9e720255b567152
Nate Fischer <ntfschr@yahoo.com>
2014-04-12 01:53:40 -0700

    Implemented inheritance structure and pickup classes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 // GLOBAL VALUES
 var STARTING_HEALTH = 100;
+var PLAYER_KEY = 'ginger';
+var PLAYER2_KEY = 'fob';
+var DRAGON_KEY = 'dragon';
+var HEROIN_KEY = 'heroin';
 
 
 
@@ -18,15 +22,32 @@ window.onload = main();
 //
 //include("assets/js/actor.js");
 
+
+
+
+
+
+
+
+
+
+
+
+
 ///////////////////////////////////
-// Actor class
+// Actor class -- virtual
 ///////////////////////////////////
 
 
-function Actor(x, y)
+function Actor(x, y, key)
 {
     this.x = x;
     this.y = y;
+    // call game sprite constructor
+    //game.add.sprite.call(this, x, y, key);
+    //this = game.add.sprite(x,y,key);
+
+
     this.isVisible = true; // default
     this.isAlive = true; // default
 }
@@ -58,30 +79,45 @@ function Actor(x, y)
 
 function Player(x, y)
 {
-    Actor.call(this, x, y);
+    Actor.call(this, x, y, PLAYER_KEY);
     this.health = STARTING_HEALTH;
 }
 
 // correct the constructor pointer because it points to Person
 Player.prototype.constructor = Player;
 
+// perhaps walk method when I can implement it
+
+///////////////////////////////////
+// Pickup class -- virtual
+///////////////////////////////////
+
+function Pickup(x, y, key)
+{
+    Actor.call(this, x, y, key);
+}
+
+///////////////////////////////////
+// Heroin class
+///////////////////////////////////
+
+function HeroinPickup(x, y, key)
+{
+    Pickup.call(this, x, y, HEROIN_KEY);
+}
 
 
-
-
-
-
-
-
-
-
-
-
+///////////////////////////////////
+// Main function
+///////////////////////////////////
 
 
 function main() {
 
-    var game = new Phaser.Game(1024, 600, Phaser.CANVAS, '', { preload: preload, create: create,update: update });
+
+
+    // declare game object as global
+    game = new Phaser.Game(1024, 600, Phaser.CANVAS, '', { preload: preload, create: create,update: update });
 
     var speed = 4;
     var audioelement = document.createElement('audio');
@@ -90,22 +126,22 @@ function main() {
         Phaser.Canvas.setSmoothingEnabled(game.context,false);
         game.stage.backgroundColor = '#ffffff';
         
-        game.load.image("heroin_syringe", 'assets/images/heroin/heroinsyringe.png');
-        game.load.image("dino", 'assets/images/other/dino.png');
-        game.load.image("player2", 'assets/images/playerV2/PlayerV2.png');
-        game.load.atlasJSONHash('ginger','assets/sprites/playerspriteatlas.png','assets/sprites/playersprite.json');
+        game.load.image(HEROIN_KEY, 'assets/images/heroin/heroinsyringe.png');
+        game.load.image(DRAGON_KEY, 'assets/images/other/dino.png');
+        game.load.image(PLAYER2_KEY, 'assets/images/playerV2/PlayerV2.png');
+        game.load.atlasJSONHash(PLAYER_KEY,'assets/sprites/playerspriteatlas.png','assets/sprites/playersprite.json');
 
 	//audioelement.setAttribute('src','assets/audio/Game_Music.mp3');
     }
 
     function create () {
 
-        heroin_syringe = game.add.sprite(500,100,'heroin_syringe');
-        dino = game.add.sprite(500,300,'dino');
+        heroin_syringe = game.add.sprite(500,100,HEROIN_KEY);
+        dino = game.add.sprite(500,300,DRAGON_KEY);
         dino.scale.y = .08;
         dino.scale.x = .08;
-        player2 = game.add.sprite(100,300,'player2');
-        m_player1 = game.add.sprite(0,200,'ginger');
+        player2 = game.add.sprite(100,300,PLAYER2_KEY);
+        m_player1 = game.add.sprite(0,200,PLAYER_KEY);
         m_player1.scale.x = .2;
         m_player1.scale.y = .2;
         m_player1.animations.add('walk');
@@ -115,8 +151,9 @@ function main() {
     }
 
     // DEBUG
-    a1 = new Actor(1,1);
+    //a1 = new Actor(1,1);
     p1 = new Player(5,1);
+    h1 = new HeroinPickup(2,1);
 
 
     function update(){
