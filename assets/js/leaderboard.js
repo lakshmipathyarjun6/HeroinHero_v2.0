8f8174be21e19239e627852499c727ebd8803870
Joraaver Chahal <machspeeds@gmail.com>
2014-04-13 08:51:05 -0700

    Leaderboard was trash

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,25 +1,3 @@
 
 
 
-$(document).ready(function(){
-    alert('go');
-    $("#submit").click(function() {
-        alert($("#username").val());
-        $.ajax({
-            type: 'POST',
-            url: 'https://api.isaacloud.com/v1/admin/leaderboards',
-            data: {
-                "active": true,
-                "cron": "0 0 1 * * *",
-                "description": "Main user leaderboard",
-                "name": "Main",
-                "script": 1
-            },
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer feff2c47c678bd66948e3327f64e9a9e") //May need to use "Authorization" instead
-            },
-            dataType: "jsonp"
-        });
-    });
-});
-
