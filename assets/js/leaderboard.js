3a228458d58ae7668e69f146ac94a8a9db318906
Joraaver Chahal <machspeeds@gmail.com>
2014-04-13 00:38:29 -0700

    Nothing really changed

---
+++ assets/js/leaderboard.js
@@ -0,0 +1,22 @@
+
+
+
+$(document).ready(function(){
+    alert('go');
+    $("#submit").click(function() {
+        alert($("#username").val());
+        
+        $.ajax({
+            type: 'POST',
+            url: 'https://api.isaacloud.com/v1/admin/leaderboards',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Bearer d05f373bd5dbdffda66923c3d9e29e") //May need to use "Authorization" instead
+            },
+            dataType: "json",
+            sucess: function(result) {
+                alert('done');
+            }
+        });
+    });
+});
+
