7527c79aa351c2200e6f8e5c5ef5eec7aeb7138a
Joraaver Chahal <machspeeds@gmail.com>
2014-04-13 05:36:42 -0700

    leaderboard stuff

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -5,14 +5,20 @@ $(document).ready(function(){
     alert('go');
     $("#submit").click(function() {
         alert($("#username").val());
-        
         $.ajax({
             type: 'POST',
             url: 'https://api.isaacloud.com/v1/admin/leaderboards',
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer d05f373bd5dbdffda66923c3d9e29e") //May need to use "Authorization" instead
+            data: {
+                "active": true,
+                "cron": "0 0 1 * * *",
+                "description": "Main user leaderboard",
+                "name": "Main",
+                "script": 1
             },
-            dataType: "json"
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Bearer feff2c47c678bd66948e3327f64e9a9e") //May need to use "Authorization" instead
+            },
+            dataType: "jsonp"
         });
     });
 });
